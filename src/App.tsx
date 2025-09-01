d09b4198824a2cc3ce9ae6e0bfc84c0e1090dbac
Yooi Ken <yooibox@gmail.com>
2025-09-01 18:38:46 +0800

    Fix github page router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import Landing from './pages/Landing'
 import Room from './pages/Room'
